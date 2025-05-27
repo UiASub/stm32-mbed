af4ee69739b2c56e43a554aa55151d754f647dd4
Simen <simenvoffa01@gmail.com>
2025-05-27 18:28:28 +0200

    Added code to thread + gitignore

---
+++ BUILD/NUCLEO_F767ZI/ARMC6/mbed-http-client/http_parser/http_parser.d
@@ -0,0 +1,4 @@
+BUILD\NUCLEO_F767ZI\ARMC6\mbed-http-client\http_parser\http_parser.o: \
+  mbed-http-client\http_parser\http_parser.c \
+  BUILD\NUCLEO_F767ZI\ARMC6\mbed_config.h \
+  mbed-http-client\http_parser\http_parser.h
--- main.cpp
+++ main.cpp
@@ -1,58 +1,71 @@
 #include "mbed.h"
 #include "EthernetInterface.h"
-#include "SocketAddress.h"
-#include "http_request.h"          // from the mbed-http-client library
+#include "TCPSocket.h"
+#include "string"
 
 /* --------- build-time network selection ------------------------------ */
+/*
 #ifndef USE_DHCP                    // let your build system override if needed
     #define USE_DHCP     1         // 1 = DHCP (default), 0 = static
 #endif
 
 #if !USE_DHCP                       // only needed in static mode
-    #define STATIC_IP      "192.168.0.50"
-    #define STATIC_NETMASK "255.255.255.0"
-    #define STATIC_GATEWAY "192.168.0.1"
+    #define STATIC_IP      "192.168.68.50"
+    #define STATIC_NETMASK "255.255.252.0"
+    #define STATIC_GATEWAY "192.168.68.1"
 #endif
+*/
+
+#define STATIC_IP      "192.168.68.60"
+#define STATIC_NETMASK "255.255.252.0"
+#define STATIC_GATEWAY "192.168.68.1"
 /* --------------------------------------------------------------------- */
+
+Thread thread_UpDownComm;
+
+void ThreadFunc_HTTP_GET() 
+{
+    EthernetInterface net;
+    while (1) 
+    {
+        printf("\n DOWNSIDE HTTP-SERVER STARTING...");                
+
+        nsapi_error_t err = net.set_network(STATIC_IP, STATIC_NETMASK, STATIC_GATEWAY);
+        if (err) 
+        {
+            printf("net.set_network() failed: %d\r\n", err);
+        }
+        printf("net.set_network() succeeded\r\n");
+
+        /*
+        err = net.connect();
+        if (err) {
+            printf("net.connect() failed: %d\r\n", err);
+            break;
+        }
+        */
+
+        /* HTTP Server ---------------------------------------------------------- */
+        TCPSocket server;
+        server.open(&net);
+        server.bind(80);
+        server.listen(5);
+        printf("Listening on port 80...\r\n");
+
+        while (1)
+        {
+            TCPSocket *client = server.accept();
+            client->set_timeout(5000);
+
+            std::string request;
+            char buf[256];
+            int total = 0;
+        }
+    }
+}
 
 int main()
 {
-    printf("\n=== NUCLEO-F767ZI HTTP-GET demo ===\r\n");
-
-    EthernetInterface net;
-    nsapi_error_t err = net.connect();
-    if (err) {
-        printf("net.connect() failed: %d\r\n", err);
-        return 1;
-    }
-
-    /* MAC address comes back as a C string */
-    printf("MAC  : %s\r\n", net.get_mac_address());
-
-    /* ask the interface to fill a SocketAddress structure */
-    SocketAddress ip;
-    net.get_ip_address(&ip);
-    printf("IP   : %s\r\n\n",
-           ip.get_ip_address() ? ip.get_ip_address() : "None");
-
-    /* HTTP GET ---------------------------------------------------------- */
-    const char *url = "http://api.ipify.org";
-
-    HttpRequest req(&net, HTTP_GET, url);
-    HttpResponse *res = req.send();
-
-    if (!res) {
-        printf("HTTP request failed: %d\r\n", req.get_error());
-    } else {
-        printf("Status: %d (%s)\r\n",
-               res->get_status_code(), res->get_status_message().c_str());
-        printf("Body:\n%s\n", res->get_body_as_string().c_str());
-    }
-
-    while (1) {
-        ThisThread::sleep_for(10);
-    }
-
-    net.disconnect();
-    printf("\nDone – interface shut down.\r\n");
+    thread_UpDownComm.start(ThreadFunc_HTTP_GET);
 }
+
